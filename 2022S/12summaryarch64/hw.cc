b57c42ee84a298ef1a768f1a89e909bb26373362
JoshuaFastert <97991360+JoshuaFastert@users.noreply.github.com>
2022-04-04 20:47:37 -0400

    Update hw.cc

--- 2022S/12summaryarch64/hw.cc
+++ 2022S/12summaryarch64/hw.cc
@@ -12,3 +12,15 @@ int main() {
 	copy(a, b, 10); // 1.
 
 }
+/*
+	.global _Z4copyPiS_i // Joshua Fastert I pledge that I have abided by the stevens honor system.
+_Z4copyPiS_i:
+start:
+	mov x4, #4
+	ldr x3, [x1], x4 // lode the first value of b into x3 than move 4 bits
+	str x3, [x0], x4 // store x3 into a
+	sub x2, x2, 1 // iterate down the length
+	cmp x2, 0 // if we have finished the array end
+	bne start // else loop
+	ret
+*/
